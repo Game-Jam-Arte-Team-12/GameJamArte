e7cc77029890ae1ecf5c2544a0d06b5cc912dd25
MaurreKiTu <alexandre.sabouni@gmail.com>
2020-03-04 22:34:57 +0100

    added gamemanager, debug gizmos (+fix)

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+    MainMenu,
+    Game,
+    Pause,
+    GameOver,
+    Win
+}
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public GameState GameState;
+
+    public bool DEV = true;
+    
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        ChangeGameState(GameState.MainMenu);
+    }
+
+    public void Play()
+    {
+        ChangeGameState(GameState.Game);
+    }
+
+    public void Pause()
+    {
+        ChangeGameState(GameState.Pause);
+    }
+
+    public void GameOver()
+    {
+        ChangeGameState(GameState.GameOver);
+    }
+
+    public void Win()
+    {
+        ChangeGameState(GameState.Win);
+    }
+
+    // Change current state of the game
+    public void ChangeGameState(GameState newState)
+    {
+        GameState = newState;
+    }
+}
---
+++ Assets/Scripts/Tools/DebugGizmos.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugGizmos : MonoBehaviour
+{
+    public Color GizColor = Color.blue;
+
+    public float Radius = .2f;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = GizColor;
+        Gizmos.DrawSphere(transform.position, Radius);
+    }
+}
