98eaa48095d54085515fb594af094ffeee7746ba
Thibaud <rintkid@hotmail.com>
2020-03-08 05:53:50 +0100

    Updated Text & Trigger Prefabs Premier level (0) rempli avec textes et triggers

---
+++ Assets/Scripts/Managers/AppearDisappearTextManager.cs
@@ -0,0 +1,109 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using TMPro;
+using DG.Tweening;
+
+public class AppearDisappearTextManager : MonoBehaviour
+{
+    private TextMeshPro _m_textMeshPro;
+    private bool _alreadyLaunched = false;
+    private bool _displayDone = false;
+    private bool _disappearDone = false;
+
+    private int visibleCount;
+
+    private float _rollInSpeed = 0.05f;
+    private float _rollOutSpeed = 0.005f;
+
+    private Tween _myTweenText;
+
+
+    IEnumerator Start()
+    {
+        _m_textMeshPro = gameObject.GetComponent<TextMeshPro>() ?? gameObject.AddComponent<TextMeshPro>();
+        _m_textMeshPro.maxVisibleCharacters = 0;
+        _m_textMeshPro.SetText("");
+        //yield return new WaitForSeconds(0.05f);
+        yield return StartCoroutine(RollIn());
+    }
+
+    public IEnumerator RollIn(float waitTime = 0)
+    {
+        yield return new WaitForSeconds(0.05f); // WAIT FOR THE TEXT TO RENDER
+        yield return new WaitForSeconds(waitTime);
+        FadeInBlock();
+        int totalVisibleCharacters = _m_textMeshPro.textInfo.characterCount;
+        Debug.Log(totalVisibleCharacters);
+        int counter = 0;
+        _displayDone = false;
+
+        while (_displayDone == false)
+        {
+            visibleCount = counter % (totalVisibleCharacters + 1);
+
+            _m_textMeshPro.maxVisibleCharacters = visibleCount; // How many chars should
+
+            if (visibleCount >= totalVisibleCharacters)
+            {
+                //yield return new WaitForSeconds(1.0f);
+                _displayDone = true;
+            }
+
+            counter += 1;
+
+            yield return new WaitForSeconds(_rollInSpeed);
+        }
+    }
+
+    private void FadeInBlock()
+    {
+        DOVirtual.Float(-1, 0, 2f, (float value) => {
+            // ObjectToDisplayOn
+            //_m_textMeshPro.alpha = value;
+            _m_textMeshPro.fontSharedMaterial.SetFloat("_FaceDilate", value);
+            //transform.position = Vector3.Lerp();
+        });
+
+        _myTweenText = DOVirtual.Float(-0.1f, 0, 2f, (float value) => {
+            // ObjectToDisplayOn
+            //_m_textMeshPro.alpha = value;
+            _m_textMeshPro.fontSharedMaterial.SetFloat("_FaceDilate", value);
+            //transform.position = Vector3.Lerp();
+        }).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+        //DOVirtual.Float(1, 0, 4f, (float value) => {
+        //    // ObjectToDisplayOn
+        //    _m_textMeshPro.fontSharedMaterial.SetFloat("_OutlineSoftness", value);
+        //    //transform.position = Vector3.Lerp();
+        //});
+    }
+
+    public IEnumerator RollOut(float waitTime = 0f)
+    {
+        yield return new WaitForSeconds(waitTime);
+        //yield return new WaitForSeconds(0.05f); // WAIT FOR THE TEXT TO RENDER
+
+        int totalVisibleCharacters = _m_textMeshPro.textInfo.characterCount;
+        int counter = totalVisibleCharacters;
+        counter = visibleCount;
+        _displayDone = true;
+        while (_disappearDone == false)
+        {
+            int visibleCount = counter % (totalVisibleCharacters + 1);
+
+            _m_textMeshPro.maxVisibleCharacters = visibleCount; // How many chars should
+
+            if (visibleCount <= 0)
+            {
+                //yield return new WaitForSeconds(1.0f);
+                _disappearDone = true;
+            }
+
+            counter -= 1;
+            yield return new WaitForSeconds(_rollOutSpeed);
+        }
+        _myTweenText.Kill();
+    }
+
+
+}
---
+++ Assets/Scripts/Managers/TextTrigger.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+
+public class TextTrigger : MonoBehaviour
+{
+    [Header("Objet Texte A Faire Apparaitre (facultatif)")]
+    public TextMeshPro ObjectToDisplayOn;
+    public float DelayToAppear = 0;
+    [TextArea]
+    public string TextToDisplay;
+
+    //public float DelayToDisplay;
+    //public string EffectToDisplay;
+    [Header("Objet Texte A Faire Disparaitre (facultatif")]
+    public TextMeshPro ObjectToDisappear;
+    public float DelayToDisappear = 0;
+
+    private bool _hasBeenTriggered = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    IEnumerator OnTriggerEnter(Collider collision)
+    {
+        //ObjectToDisplayOn.GetComponent<RollingTextAppear>().RollBack():
+        if(collision.tag == "Player" && _hasBeenTriggered == false)
+        {
+            _hasBeenTriggered = true;
+            if (ObjectToDisappear != null)
+            {
+                yield return StartCoroutine(ObjectToDisappear.GetComponent<AppearDisappearTextManager>().RollOut(DelayToDisappear));
+            }
+            if (ObjectToDisplayOn != null)
+            {
+                ObjectToDisplayOn.maxVisibleCharacters = 0;
+                ObjectToDisplayOn.SetText(TextToDisplay);
+                yield return StartCoroutine(ObjectToDisplayOn.GetComponent<AppearDisappearTextManager>().RollIn(DelayToAppear));
+            }
+            Destroy(this.gameObject);
+        }
+        //yield return StartCoroutine(ObjectToDisplayOn.GetComponent<RollingTextAppear>().RollBack());
+    }
+
+    //public string LaunchTextDisplay(string _textToDisplay)
+    //{
+    //    ObjectToDisplayOn.SetText(_textToDisplay);
+    //    DOVirtual.Float(0, 1, 2f, (float value) => {
+    //        // ObjectToDisplayOn
+    //        ObjectToDisplayOn.alpha = value;
+
+    //        //transform.position = Vector3.Lerp();
+    //    });
+    //    string Name = "Cool";
+    //    return Name;
+
+    //}
+
+}
